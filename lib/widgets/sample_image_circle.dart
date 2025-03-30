5c26e6af00e5ae07cd42cc99d212accf692f0c26
fattsowavy <fatwaaf66@gmail.com>
2025-03-30 17:36:24 +0800

    avatar circle  widget

--- lib/widgets/sample_image_circle.dart
+++ lib/widgets/sample_image_circle.dart
@@ -16,7 +16,53 @@ class SampleImageCircle extends StatelessWidget {
         ),
         backgroundColor: Colors.blue,
       ),
-
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset("assets/images/sample-image.jpg"),
+            SizedBox(height: 20),
+            Text("Ini Pake Circle Avatar"),
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage("assets/images/sample-image.jpg"),
+            ),
+            SizedBox(height: 30),
+            Text("Ini Pake ClipRRect"),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(50),
+              child: Image.asset(
+                "assets/images/sample-image.jpg",
+                height: 100,
+                width: 100,
+                fit: BoxFit.fill,
+              ),
+            ),
+            SizedBox(height: 30),
+            Text("Ini Pake Clip Oval"),
+            ClipOval(
+              child: Image.asset(
+                  "assets/images/sample-image.jpg",
+                height: 100,
+                width: 100,
+                fit: BoxFit.fill,
+              ),
+            ),
+            SizedBox(height: 30),
+            Text("Ini Pake Container"),
+            Container(
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                    image: AssetImage("assets/images/sample-image.jpg"),
+                    fit: BoxFit.fill,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
